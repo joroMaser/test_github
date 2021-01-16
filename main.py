55f382ca6683344d1b313b781b1ee0d742fa7eff
test_me <test@test.com>
2021-01-16 20:06:11 +0200

    add new text

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('hello world!')
+def main():
+    print('hello world!')
+    from auth import login
+    if login:
+        print('hi!')
+        print('new text')
+        
+if __name__ == '__main__':
+    main()
